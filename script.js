6cae3fa4f26b9a9d2268e16e67e72ef75b40539e
Gabriel Tavares <gabrieltavares.crvg@gmail.com>
2025-02-13 17:15:36 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function togglemode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+
+    /* if (html.classList.contains('light')) {
+        html.classList.remove('light')
+    } else {
+        html.classList.add('light')
+    } */
+
+    /*substituir a imagem no light e dark mode*/
+
+    const img = document.queryselector("#profile img") 
+
+        if (html.classList.contains("light")) {
+            img.setAttribute("src", "./assets/avatar-light.png")
+        }  else {
+            img.setAttribute("src", "./assets/avatar.png")
+        }
+        
+}
